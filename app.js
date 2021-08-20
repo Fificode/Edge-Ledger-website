1fba1acbe3de8c45e833aaaf78ac9e8796fcb231
Firdaws <lamidifirdaws@gmail.com>
2021-08-20 20:04:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const hamburgerNav = document.getElementById("hamburger-icon");
+function toggleVerticalbar() {
+  if (nav.style.display === 'none') {
+    nav.style.display = "block";
+  }
+  else {
+    nav.style.display = "none";
+  }
+}
